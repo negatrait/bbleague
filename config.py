773596a382fc5f649c1e45168079cda74e074ed4
Harri Ahokas <harri.ahokas@gmail.com>
2025-03-06 19:32:43 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import os
+import secrets
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    MYSQL_HOST = 'negatrait.mysql.pythonanywhere-services.com'
-    MYSQL_USER = 'negatrait'
-    MYSQL_PASSWORD = 'bbleaguepassword'
-    MYSQL_DB = 'default'
+    # Generate a random secret key if one doesn't exist
+    SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_hex(16)
+    MYSQL_HOST = os.environ.get('MYSQL_HOST') or 'negatrait.mysql.pythonanywhere-services.com'
+    MYSQL_USER = os.environ.get('MYSQL_USER') or 'negatrait'
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD') or 'bbleaguepassword'
+    MYSQL_DB = os.environ.get('MYSQL_DB') or 'default'
