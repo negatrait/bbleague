3d042e46b20c21ba18ae4b5dcd4c51b7147d3151
Harri Ahokas <harri.ahokas@gmail.com>
2025-03-06 11:30:15 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'your-username'
-    MYSQL_PASSWORD = 'your-password'
-    MYSQL_DB = 'bbroster'
+    MYSQL_HOST = 'negatrait.mysql.pythonanywhere-services.com'
+    MYSQL_USER = 'negatrait'
+    MYSQL_PASSWORD = 'bbleaguepassword'
+    MYSQL_DB = 'default'
