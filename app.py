7a7d80c0d676f3fb28ba726e0401fd6a8596daa6
Harri Ahokas <harri.ahokas@gmail.com>
2025-03-06 11:13:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import mysql.connector
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+def get_db_connection():
+    conn = mysql.connector.connect(
+        host=app.config['MYSQL_HOST'],
+        user=app.config['MYSQL_USER'],
+        password=app.config['MYSQL_PASSWORD'],
+        database=app.config['MYSQL_DB']
+    )
+    return conn
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+def home():
+    return render_template('index.html')
 
+@app.route('/create_team', methods=['GET', 'POST'])
+def create_team():
+    if request.method == 'POST':
+        team_name = request.form['team_name']
+        # Add code to save team to database
+        return redirect(url_for('home'))
+    return render_template('create_team.html')
+
+@app.route('/view_team/<int:team_id>')
+def view_team(team_id):
+    # Add code to retrieve team from database
+    return render_template('view_team.html', team=team)
+
+if __name__ == '__main__':
+    app.run(debug=True)
