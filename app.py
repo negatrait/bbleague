dbffb2e7ee08ebc0e9ba03f350f6240f864b4b8f
Harri Ahokas <harri.ahokas@gmail.com>
2025-03-07 13:10:53 +0200

    bbleague log

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.config.from_object(Config)
 # Setup logging
 logging.basicConfig(level=logging.DEBUG,
                     handlers=[
-                        logging.FileHandler("your_log_file.log"),
+                        logging.FileHandler("bbleague.log"),
                         logging.StreamHandler()
                     ])
 logger = logging.getLogger(__name__)
