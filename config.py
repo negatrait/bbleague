43eac0b277d359fda0ba58e8bd353ad7c419715d
Harri Ahokas <harri.ahokas@gmail.com>
2025-03-07 16:04:03 +0200

    default to default

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ class Config:
     MYSQL_HOST = os.environ.get('MYSQL_HOST') or 'localhost'
     MYSQL_USER = os.environ.get('MYSQL_USER') or 'root'
     MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD') or 'password'
-    MYSQL_DB = os.environ.get('MYSQL_DB') or 'bbleague'
+    MYSQL_DB = os.environ.get('MYSQL_DB') or 'default'
